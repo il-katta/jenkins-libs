8e2eca255e0c3aadf1352d432ff0a08ac8227856
Andrea Cattaneo <andrea.cattaneo@infoesse.it>
2019-03-01 15:25:24 +0100

    write_build_info

--- vars/nuget_download.groovy
+++ vars/nuget_download.groovy
@@ -1,24 +1,27 @@
 #!/usr/bin/groovy
 
+@groovy.transform.Field
+def nuget_url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
 def call() {
     def ret = 0
     if (isUnix()) {
         ret = sh(
             returnStatus: true, 
-            script: '''
-                curl -o nuget.exe "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-            '''
+            script: """
+                curl -o \"${workspace}/nuget.exe\" \"${nuget_url}\"
+            """
         )
     } else {
         ret = powershell(
             returnStatus: true, 
-            script: '''
-                $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-                $targetNugetExe = "nuget.exe"
+            script: """
+                $sourceNugetExe = \"${nuget_url}\"
+                $targetNugetExe = \"nuget.exe\"
                 Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
                 Set-Alias nuget $targetNugetExe -Scope Global -Verbose
                 exit 0
-            '''
+            """
         )
     }
     if (ret == 0) {
---
+++ vars/write_build_info.groovy
@@ -0,0 +1,48 @@
+#!/usr/bin/groovy
+import groovy.json.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+String get_date_string() {
+    def dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm")
+    return dateFormat.format(new Date())
+}
+
+LinkedHashMap get_data() {
+    def data = [:]
+    data.BRANCH_NAME = env.BRANCH_NAME
+    data.BUILD = env.BUILD_TAG
+    data.COMMIT = env.GIT_COMMIT
+    data.BANCH = env.GIT_BRANCH
+    data.DATE = get_date_string()
+    return data
+}
+
+def to_json_file(String fileName) {
+    def data = get_data()
+    def jsonStr = groovy.json.JsonOutput.toJson(data)
+    writeFile file: fileName, text: json, encoding: 'UTF-8'
+
+    // non funziona ( java.lang.UnsupportedOperationException: must specify $class with an implementation of interface net.sf.json.JSON )
+    // writeJSON file: fileName, json: data, pretty: true 
+
+    /* 
+    // non funziona ( java.io.NotSerializableException: groovy.json.JsonBuilder )
+
+    def builder = new JsonBuilder(data)
+
+    json_str = builder.toPrettyString()
+
+    print json_str
+
+    writeFile file: fileName, text: json_str, encoding: 'UTF-8'
+    */
+}
+
+def to_yaml_file(String fileName) {
+    def data = get_data()
+
+    print data.toString()
+
+    writeYaml file: fileName, data: data
+}
